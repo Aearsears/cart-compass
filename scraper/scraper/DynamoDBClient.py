84d3dbd71488e5583ff719551b2addd11d7458f9
Aearsears <53102185+Aearsears@users.noreply.github.com>
2025-02-02 03:23:28 +0000

    create ddb table

--- scraper/scraper/DynamoDBClient.py
+++ scraper/scraper/DynamoDBClient.py
@@ -3,6 +3,7 @@ import boto3
 
 class DynamoDBClient():
     def __init__(self, *, region, endpoint, access_key, secret) -> None:
+        self.table_name = "ProductPrices"
         self.client = boto3.client(
             'dynamodb',
             region_name=region,
@@ -10,3 +11,34 @@ class DynamoDBClient():
             aws_access_key_id=access_key,
             aws_secret_access_key=secret
         )
+        table = self.client.describe_table(
+            TableName=self.table_name).get("Table")
+
+        if not table:
+            self.create_table()
+
+    def create_table(self):
+        self.client.create_table(AttributeDefinitions=[
+            {
+                'AttributeName': 'UPC',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'Date',
+                'AttributeType': 'S'
+            },
+        ],
+            TableName='string',
+            KeySchema=[
+            {
+                'AttributeName': 'UPC',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'Date',
+                'KeyType': 'RANGE'
+            },
+        ])
+
+    def put(self, item):
+        self.client.put_item(TableName=self.table_name, **item)
