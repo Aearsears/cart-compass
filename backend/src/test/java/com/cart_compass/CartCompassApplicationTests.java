6ea11a5923831b03cd8e8e545730a971c221e5fa
Aearsears <53102185+Aearsears@users.noreply.github.com>
2025-02-11 17:59:36 -0500

    start backend

---
+++ backend/src/test/java/com/cart_compass/CartCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cart_compass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartCompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
